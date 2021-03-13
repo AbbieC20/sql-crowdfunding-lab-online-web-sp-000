b669e3b1edb2142dad445d4052b55b7b16a4a89f
remote-torrent-6747 <abbyc@sky.com>
2021-03-13 15:02:26 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -12,6 +12,7 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "Write your SQL query Here"
+  # ["Iguana", 10], ["Swizzle", 12], ["Sirius", 19], ["Alex", 20], ["Marisa", 24], ["Voldemort", 34]
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
